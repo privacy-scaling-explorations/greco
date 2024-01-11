73f7e14a3add34c38c16b655201be8358fdab9ca
enrico.eth <enricobottazzi@icloud.com>
2024-01-11 17:24:46 +0100

    chore: update Apis

--- circuit.py
+++ circuit.py
@@ -1,8 +1,6 @@
-from bfv.crt import Q
-from bfv.bfv import BFV
+from bfv.crt import CRTModuli
+from bfv.bfv import BFV, RLWE
 from bfv.discrete_gauss import DiscreteGaussian
-from bfv.polynomial import poly_mul, poly_add, Polynomial
-from utils import mod_inverse
 
 
 def main():
@@ -28,47 +26,30 @@ def main():
         1152921504581419009,
         1152921504580894721,
     ]
-    q = Q(qis)
+    crt_moduli = CRTModuli(qis)
     t = 65537
     n = 1024
     sigma = 3.2
     discrete_gaussian = DiscreteGaussian(sigma)
 
-    bfv = BFV(n, q.q, t, discrete_gaussian)
+    rlwe = RLWE(n, crt_moduli.q, t, discrete_gaussian)
+    bfv = BFV(rlwe)
 
     # s is the secret key polynomial, sampled from the ternary distribution {-1, 0, 1}
     # e is the error polynomial, sampled from the discrete Gaussian distribution
     # m is the message polynomial, sampled from the plaintext space
-    s = bfv.SampleFromTernaryDistribution()
-    e = bfv.SampleFromErrorDistribution()
-    m = bfv.Rt.sample_polynomial()
-
-    # k1 is equal to m * q mod t
-    k1 = poly_mul(m.coefficients, [q.q])
-    k1 = Polynomial(k1, bfv.Rt)
+    secret_key = bfv.rlwe.SampleFromTernaryDistribution()
+    e = bfv.rlwe.SampleFromErrorDistribution()
+    message = bfv.rlwe.Rt.sample_polynomial()
 
     # ciphertexts is a list of ciphertexts, each ciphertext is a tuple (ct0, ct1) generated via secret key encryption in RNS basis
     ciphertexts = []
 
     # perform secret key encryption on m in RNS basis
     for qi in qis:
-        bfv_i = BFV(n, qi, t, discrete_gaussian)
-
-        # a_i is a polynomial sampled from Rqi (Ring of polynomials with coefficients in Zqi modulo the cyclotomic polynomial)
-        a_i = bfv_i.Rq.sample_polynomial()
-
-        # k0 is equal to the negative of the multiplicative inverse of t modulo qi
-        k0_i = mod_inverse(t, qi) * (-1)
-
-        # Perform encryption : ct0 = a_i * s + e + k0 * k1 mod qi | ct1 = a_i mod qi
-        a_i_times_s = poly_mul(a_i.coefficients, s.coefficients)
-        a_i_times_s_plus_e = poly_add(a_i_times_s, e.coefficients)
-        k0_times_k1_i = poly_mul([k0_i], k1.coefficients)
-        ct0_i = poly_add(a_i_times_s_plus_e, k0_times_k1_i)
-        ct0_i = Polynomial(ct0_i, bfv_i.Rq)
-        ciphertext_i = (ct0_i, a_i)
-
-        ciphertexts.append(ciphertext_i)
+        bfv_rqi = BFV(RLWE(n, qi, t, discrete_gaussian))
+        ciphertext = bfv_rqi.SecretKeyEncrypt(secret_key, message, e, crt_moduli.q)
+        ciphertexts.append(ciphertext)
 
     # PHASE 1
     # Pass Si as input to the circuit (private)
@@ -89,10 +70,3 @@ def main():
 
 
 main()
-
-
-# Questions
-# - Error accumulation when working with `crt`
-
-# RLWE!
-# add message encoding!
